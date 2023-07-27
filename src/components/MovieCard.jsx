7edcb9a60f228d704cae2f4b3bc9f4257c2e7f03
Shrijan Maharjan <srijinx@gmail.com>
2023-07-27 08:48:18 +0545

    build issue

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState } from 'react'
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function MovieCard(props) {
   const [genreArr, setGenreArr] = useState(null);
